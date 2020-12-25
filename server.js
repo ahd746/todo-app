4367525147a763662e8be1284539d55c7236e70a
Ahmed Salama <eng.ahd746@gmail.com>
2020-12-24 20:52:29 -0500

    V 1.0

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 app.use('/api', router);
-app.use(express.static('client/build'))
+app.use(express.static('client/build'));
 app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
 
 //Set the port and listen 
